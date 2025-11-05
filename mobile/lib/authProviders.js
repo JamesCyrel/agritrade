2b781b38c362aa4d9d7c4bad95208d7614f9efb4
hanyu <nahoynikulcallanta@gmail.com>
2025-11-05 11:21:10 +0800

    Stop tracking sensitive files

--- mobile/lib/authProviders.js
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_CLIENT_ID = "799556952675-3vr5nbfvme469pq8j1c85mrdf2h2ue86.apps.googleusercontent.com";
-
-export const GOOGLE_WEB_CLIENT_ID = "799556952675-qbggqtvum1gi8k2ldas9gvtaqf93aooq.apps.googleusercontent.com"
